3ce86265830e6b5165a5948f12a2dbc328c8b5fb
David Dinh <dmdinh22@gmail.com>
2014-11-07 01:24:16 -0800

    final edit

--- rpsls.rb
+++ rpsls.rb
@@ -3,47 +3,37 @@
 # 2. compare: paper > rock, paper > spock, rock > scissors,  rock > lizard, scissors > paper, scissors > lizard, lizard > spock, lizard > paper, spock > scissors, spock > rock, or tie if same
 # 3. play again?
 
-PLAY = {'r' => 'rock', 'p' => 'paper', 's' => 'scissors', 'l' => 'lizard', 'sp' => 'spock'}
+require 'pry'
 
-def print_out_result_message(player_action, computer_action)
-  puts "You chose #{player_action} and computer chose #{computer_action}"
-  if player_action == computer_action
-    puts "It's a tie."
-  elsif player_action == 'r' && computer_action == 's'
-    puts "Your rock crushes computer's scissors."
-  elsif player_action == 'r' && computer_action == 'l'
-    puts "Your rock crushes computer's lizard."
-  elsif player_action == 'p' && computer_action == 'r'
-    puts "Your paper covers computer's rock."
-  elsif player_action == 'p' && computer_action == 'sp'
-    puts "Your paper disproves computer's Spock."
-  elsif player_action == 's' && computer_action == 'p'
-    puts "Your scissors cuts computers paper."
-  elsif player_action == 's' && computer_action == 'l'
-    puts "Your scissors decapitates computer's lizard."
-  elsif player_action == 'l' && computer_action == 'sp'
-    puts "Your lizard eats computer's paper."
-  elsif player_action == 'l' && computer_action == 'p'
-    puts "Your lizard poisons computer's Spock."
-  elsif player_action == 'sp' && computer_action == 's'
-    puts "Your Spock smashes computer's scissors."
-  elsif player_action == 'sp' && computer_action == 'r'
-    puts "Your Spock vaporizes computer's rock."
-  else 
-    puts "Computer won!"
-  end
-end
+PLAY = [ "rock", "paper", "scissors", "lizard", "spock"]
+
+scenario = { rock: { rock: "It's a tie!", paper: "Paper covers rock!", scissors: "Rock crushes scissors!", lizard: "Rock crushes lizard!", spock: "Spock vaporizes rock!"}, 
+
+  paper: { rock: "Paper covers rock!", paper: "It's a tie!", scissors: "Scissors cuts paper!", lizard: "Lizard eats paper!", spock: "Paper disproves Spock!"}, 
+
+  scissors: { rock: "Rock crushes scissors!", paper: "Scissors cuts paper!", scissors: "It's a tie!", lizard: "Scissors decapitates lizard!", spock: "Spock dulls scissors!"}, 
+
+  lizard: { rock: "Rock crushes lizard!", paper: "Lizard eats paper!", scissors: "Scissors decapitates lizard!", lizard: "It's a tie!", spock: "Lizard poisons Spock!"}, 
+
+  spock: { rock: "Spock vaporizes rock!", paper: "Paper disproves Spock!", scissors: "Spock dulls scissors!", lizard: "Lizard poinsons Spock!", spock: "It's a tie!"}
+}
 
 puts "Let's play Rock, Paper, Scissors, Lizard, Spock!"
 
 loop do
   begin
-  puts "Please enter 'r' for rock, 'p' for paper, 's' for scissors, 'l' for lizard, and 'sp' for Spock."
-  player_action = gets.chomp.downcase
-  end until PLAY.keys.include?(player_action)
+    puts "Please enter rock, paper, scissors, lizard, or Spock."
+    player = gets.chomp.downcase
+  end until PLAY.include?(player)
+  
+  human = player.to_sym
+  computer = PLAY.sample.to_sym
+  
+  puts "You picked #{human}."
+  puts "Computer picked #{computer}."
 
-  computer_action = PLAY.keys.sample
-  print_out_result_message(player_action, computer_action)
+  puts scenario[human][computer]
+
   puts "Would you like to play again? Enter 'y' for yes, any other key to exit."
   break if gets.chomp.downcase != 'y'
 end
