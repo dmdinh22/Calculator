95318e6987bdbb5f7216c249b38ddcba4088efe0
David Dinh <dmdinh22@gmail.com>
2014-11-13 01:02:21 -0800

    final revisions

--- ttt3v2.rb
+++ ttt3v2.rb
@@ -5,40 +5,40 @@ WINNING_LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
 X = "X"
 O = "O"
 
-def available_spaces(space)
-  space.select {|k,v| v == " "}.keys
+def available_spaces(spaces)
+  spaces.select {|k,v| v == " "}.keys
 end
 
-def draw_board(space)
+def draw_board(spaces)
   system 'clear'
-  puts "#{space[1]}|#{space[2]}|#{space[3]}"
+  puts "#{spaces[1]}|#{spaces[2]}|#{spaces[3]}"
   puts "-----"
-  puts "#{space[4]}|#{space[5]}|#{space[6]}"
+  puts "#{spaces[4]}|#{spaces[5]}|#{spaces[6]}"
   puts "-----"
-  puts "#{space[7]}|#{space[8]}|#{space[9]}"
+  puts "#{spaces[7]}|#{spaces[8]}|#{spaces[9]}"
 end
 
-def player(space)
-  if available_spaces(space).any?
-    puts "Choose an available space from #{available_spaces(space)}."
+def player(spaces)
+  if available_spaces(spaces).any?
+    puts "Choose an available spaces from #{available_spaces(spaces)}."
     position = gets.chomp.to_i
-      if available_spaces(space).include?(position)
-        space[position] = "X"
+      if available_spaces(spaces).include?(position)
+        spaces[position] = "X"
       else
-        player(space)
+        player(spaces)
       end
-    draw_board(space)
+    draw_board(spaces)
   end
 end
 
-def computer(line, space)
+def computer(line, spaces)
   defend_this_space = nil
   attacked = false
 
   WINNING_LINES.each do |l|
-    defend_this_space = two_in_a_row({1[0] => space[1[0]], 1[1] => space[1[1]], 1[2] => space[1[2]]}, O)
+    defend_this_space = two_in_a_row({1[0] => spaces[1[0]], 1[1] => spaces[1[1]], 1[2] => spaces[1[2]]}, O)
     if defend_this_space
-      space[defend_this_space] = O
+      spaces[defend_this_space] = O
       attacked = true
       break
     end
@@ -46,17 +46,17 @@ def computer(line, space)
 
   if attacked == false
     WINNING_LINES.each do |l|
-      defend_this_space = two_in_a_row({1[0] => space[1[0]], 1[1] => space[1[1]], 1[2] => space[1[2]]}, X)
+      defend_this_space = two_in_a_row({1[0] => spaces[1[0]], 1[1] => spaces[1[1]], 1[2] => spaces[1[2]]}, X)
       if defend_this_space
-        space[defend_this_space] = O
+        spaces[defend_this_space] = O
         attacked = true
         break
       end
     end
   end
 
-  space[available_spaces(space).sample] = O unless defend_this_space
-  draw_board(space)
+  spaces[available_spaces(spaces).sample] = O unless defend_this_space
+  draw_board(spaces)
 end
 
 def two_in_a_row(hash, marker)
@@ -67,11 +67,11 @@ def two_in_a_row(hash, marker)
   end
 end
 
-def check_for_winner(line, space)
-  if line.find {|l| l.all? {|k| space[k] == X} }
+def check_for_winner(line, spaces)
+  if line.find {|l| l.all? {|k| spaces[k] == X} }
     puts "YOU WIN!"
     true
-  elsif line.find {|l| l.all? {|k| space[k] == O} }
+  elsif line.find {|l| l.all? {|k| spaces[k] == O} }
     puts "Computer won, you LOSE!"
     true
   end
