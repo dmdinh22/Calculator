aa82dfae37c18dedcd0def193e28d294372174f7
David Dinh <dmdinh22@gmail.com>
2014-11-15 02:10:20 -0800

    Refactored code

--- reverse_madlibs/example.rb
+++ reverse_madlibs/example.rb
@@ -1,19 +1,22 @@
 # ruby example.rb <file name>
 
-nouns = File.open('nouns.txt', 'r') do |f|
-  f.read
-end.split
-
 # opening files in do/end block, it will automatically close it
+def get_words_from_file(file_name)
+  if !File.exists?(file_name)
+    say "#{file_name} doesn't exist!"
+    return
+  end
 
-verbs = File.open('verbs.txt', 'r') do |f|
-  f.read
-end.split
+  File.open(file_name, 'r') do |f|
+    f.read
+  end.split
+end
 
-adjectives = File.open('adjectives.txt', 'r') do |f|
-  f.read
-end.split
+nouns = get_words_from_file('nouns.txt')
 
+verbs = get_words_from_file('verbs.txt')
+
+adjectives = get_words_from_file('adjectives.txt')
 
 dictionary = {
   nouns: nouns,
@@ -35,7 +38,6 @@ def get_input(word)
   STDIN.gets.chomp
 end
 
-
 exit_with("No input file!") if ARGV.empty?
 exit_with("Input file does not exist.") if !File.exists?(ARGV[0])
 
@@ -44,9 +46,9 @@ contents = File.open(ARGV[0], 'r') do |f|
 end
 
 # contents.gsub!('NOUN', dictionary[:nouns].sample)
-
 contents.gsub!('NOUN').each do |noun|
-  get_input(noun)
+  #get_input(noun)
+  dictionary[:nouns].sample
 end
 
 contents.gsub!('VERB').each do 
